4744a4e85f80d8889209764b74c6fda39c159490
Nguyen Tien Dat <datnt.it.pro@gmail.com>
2018-08-25 03:41:30 +0700

    [Course] Apply Course page

--- src/app/admin/services/admin.service.ts
+++ src/app/admin/services/admin.service.ts
@@ -1,7 +1,15 @@
+import { CourseModel } from './admin.service';
 import { Injectable } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SharedService } from '../../shared/shared.service';
+
+export interface CourseModel {
+  id?: number;
+  name?: string;
+  cost?: number;
+  description?: string;
+}
 
 @Injectable()
 export class AdminService extends SharedService {
@@ -12,5 +20,22 @@ export class AdminService extends SharedService {
   */
   public getListCourse(): Observable<any> {
     return this.get(this.BASE_URL + 'course/');
+  }
+
+  public editCourse(id, course: CourseModel): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    params = params.append('id', id);
+    return this.post(this.BASE_URL + 'course/', course, params);
+  }
+
+  public addCourse(course: CourseModel): Observable<any> {
+    return this.post(this.BASE_URL + 'course/', course);
+  }
+
+  public deleteCourse(id): Observable<any> {
+    let params: HttpParams = new HttpParams();
+    params = params.append('id', id);
+    params = params.append('type', 'delete');
+    return this.get(this.BASE_URL + 'course/', params);
   }
 }
