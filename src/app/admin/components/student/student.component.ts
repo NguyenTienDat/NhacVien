4dc684aeb5ba47d0ceeb662d2bb664cf22570b61
Nguyen Tien Dat <datnt.it.pro@gmail.com>
2018-08-28 01:07:35 +0700

    [Student] Master dont demo

--- src/app/admin/components/student/student.component.ts
+++ src/app/admin/components/student/student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService, StudentModel } from '../../services/admin.service';
 import { SharedUiService, ToastType } from '../../../shared/shared-ui.service';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './student.component.html',
@@ -10,8 +11,6 @@ export class StudentComponent implements OnInit {
 
   public listItemsDisplay: StudentModel[] = [];
   public isPopupStudentVisible = false;
-  public selectedStudent: StudentModel;
-  public isEditMode = false;
 
   constructor(
     private adminService: AdminService,
@@ -35,42 +34,24 @@ export class StudentComponent implements OnInit {
     console.log(nane, e);
   }
 
-
-  public editStudent(classModel: StudentModel) {
-    console.log(classModel);
-    if (!classModel || !classModel.name || classModel.name.trim() === '') {
-      this.sharedUiService.showToast('Bạn chưa nhập tên lớp học', ToastType.warning);
-      return;
-    }
-    if (!classModel.start_date) {
-      this.sharedUiService.showToast('Bạn nhập sai định dạng ngày hoặc ngày bắt đầu sau ngày kết thúc', ToastType.warning);
-      return;
-    }
-    if (this.isEditMode) {
-      if (this.selectedStudent && this.selectedStudent.id) {
-        this.adminService.editStudent(this.selectedStudent.id, classModel).subscribe(res => {
-          console.log(res);
-          if (res && res.message === 'success') {
-            this.refreshAndToast('Đã lưu dữ liệu thành công!');
-            this.isPopupStudentVisible = false;
-          }
-        }, err => {
-          console.log(err);
-          this.sharedUiService.showToast('Có lỗi khi lưu dữ liệu', ToastType.error);
-        });
-      }
+  public editStudent(e, isEditMode) {
+    const student: StudentModel = e.data;
+    let editObservable: Observable<any>;
+    if (isEditMode) {
+      editObservable = this.adminService.editStudent(e.key, student);
     } else {
-      this.adminService.addStudent(classModel).subscribe(res => {
-        console.log(res);
-        if (res && res.message === 'success') {
-          this.refreshAndToast('Đã thêm lớp học thành công!');
-          this.isPopupStudentVisible = false;
-        }
-      }, err => {
-        console.log(err);
-        this.sharedUiService.showToast('Có lỗi khi lưu dữ liệu', ToastType.error);
-      });
+      editObservable = this.adminService.addStudent(student);
     }
+    editObservable.subscribe(res => {
+      console.log(res);
+      if (res && res.message === 'success') {
+        this.refreshAndToast('Đã lưu dữ liệu thành công!');
+        this.isPopupStudentVisible = false;
+      }
+    }, err => {
+      console.log(err);
+      this.sharedUiService.showToast('Có lỗi khi lưu dữ liệu', ToastType.error);
+    });
   }
 
   private refreshAndToast(message?: string) {
@@ -80,34 +61,17 @@ export class StudentComponent implements OnInit {
     setTimeout(() => this.search(), this.sharedUiService.DELAY_TIME_RELOAD);
   }
 
-  public showEditPopup(classModel: StudentModel, isEditMode: boolean) {
-    this.selectedStudent = classModel;
-    this.isEditMode = isEditMode;
-    setTimeout(this.isPopupStudentVisible = true, 200);
-  }
-
-  public showConfirmDelete(classModel: StudentModel) {
-    console.log(classModel);
-    if (!classModel || !classModel.id || !classModel.name) {
-      this.sharedUiService.showToast('Dữ liệu không chính xác', ToastType.error);
-      return;
-    }
-    const confirmDelete = this.sharedUiService.showConfirm(`
-      Bạn thực sự muốn xóa lớp học: "${classModel.name}" ?
-    `, 'XÓA LỚP HỌC');
-    confirmDelete.then(confirm => {
-      if (confirm) {
-        this.adminService.deleteStudent(classModel.id).subscribe(res => {
-          console.log(res);
-          if (res && res.message === 'success') {
-            this.refreshAndToast('Đã xóa lớp học thành công!');
-            this.isPopupStudentVisible = false;
-          }
-        }, err => {
-          console.log(err);
-          this.sharedUiService.showToast('Có lỗi khi xóa dữ liệu', ToastType.error);
-        });
+  public deleteStudent(e) {
+    console.log(e);
+    this.adminService.deleteStudent(e.key).subscribe(res => {
+      console.log(res);
+      if (res && res.message === 'success') {
+        this.refreshAndToast('Đã xóa học viên thành công!');
+        this.isPopupStudentVisible = false;
       }
+    }, err => {
+      console.log(err);
+      this.sharedUiService.showToast('Có lỗi khi xóa dữ liệu', ToastType.error);
     });
   }
 
